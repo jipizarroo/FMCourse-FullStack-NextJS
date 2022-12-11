18265bf48b964d8f46cb082d7e718700afe009b1
Javier <jipizarroo@gmail.com>
2022-12-10 23:55:54 -0300

    feat: now we have cards and artists

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,52 @@
-const Home = () => {
-  return <div>Home</div>;
+import { Box, Text, Flex } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
+
+const Home = ({ artists }) => {
+  return (
+    <GradientLayout
+      color="red"
+      subtitle="profile"
+      title="Javier Pizarro"
+      description="15 public playlists"
+      image="https://frontendmasters.github.io/fullstack-app-next-website/images/profile.png"
+      roundImage
+    >
+      <Box color="white" paddingX="40x">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artis this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/200/287"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+    </GradientLayout>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
